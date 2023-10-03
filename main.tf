92bf83e5beca8b9b276a2b8346a3e2962924adb5
HwanYoung-Cho <93585825+HwanYoung-Cho@users.noreply.github.com>
2023-10-03 14:03:38 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -126,13 +126,3 @@ resource "aws_instance" "tfdemo" {
     Billable   = "Finance teams"
   }
 }
-check "health_check" {
-  data "http" "terraform_io" {
-    url = "https://www.terraform.io"
-  }
-
-  assert {
-    condition = data.http.terraform_io.status_code == 200
-    error_message = "${data.http.terraform_io.url} returned an unhealthy status code"
-  }
-}
